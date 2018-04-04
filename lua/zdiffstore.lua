fe20b8015147859354063a3fa707d0cde22bb3dd
Dan VerWeire <dverweire@gmail.com>
2018-04-03 22:16:36 -0400

    lua: optimizations

--- lua/zdiffstore.lua
+++ lua/zdiffstore.lua
@@ -2,6 +2,6 @@ local intersectCount = redis.call("zinterstore", "temp", 2, KEYS[2], KEYS[3], "w
 local unionCount = redis.call("zunionstore", KEYS[1], 2, KEYS[2], "temp", "weights", 1, -1)
 local removedCount = redis.call("zremrangebyscore", KEYS[1], 0, 0)
 local res = redis.call("del", "temp")
-local count = redis.call("zcount", KEYS[1], "-inf", "+inf")
+local count = redis.call("zcard", KEYS[1])
 
 return count
--- lua/zduplicatescorestore.lua
+++ lua/zduplicatescorestore.lua
@@ -1,21 +1,29 @@
-local res = redis.call("zrange", KEYS[2], ARGV[1], ARGV[2], "withscores")
+local function massive_redis_command(command, key, t)
+    local i = 1
+    local temp = {}
+    local lastScore = nil
+    while(i <= #t) do
+        if lastScore == t[i + 1] then
+          table.insert(temp, t[i + 1])
+          table.insert(temp, t[i])
+        end
 
+        if #temp >= 4000 then
+            redis.call(command, key, unpack(temp))
+            temp = {}
+        end
+
+        lastScore = t[i + 1]
+        i = i + 2
+    end
+    if #temp > 0 then
+        redis.call(command, key, unpack(temp))
+    end
+end
+local res = redis.call("zrange", KEYS[2], ARGV[1], ARGV[2], "withscores")
 if next(res) ~= nil then
   redis.call('del', KEYS[1])
 end
-
-local i = 1
-local lastScore = nil
-
-while(i <= #res) do
-  if lastScore == res[i + 1] then
-    redis.call('zadd', KEYS[1], res[i + 1], res[i])
-  end
-
-  lastScore = res[i + 1]
-  i = i + 2
-end
-
+massive_redis_command("zadd", KEYS[1], res)
 local count = redis.call('zcard', KEYS[1])
-
 return count
